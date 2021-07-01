2c9cef4ae9200cc428b09479c5604a74ca5497ed
Juggalo187 <77017649+Juggalo187@users.noreply.github.com>
2021-07-01 17:43:47 -0500

    changed event to PLAYER_EVENT_ON_GIVE_XP because it seemed like when pets got last hit on an enemy player didn't get credit for the kill.

--- extra_gold_xp_spawn_onkill.lua
+++ extra_gold_xp_spawn_onkill.lua
@@ -7,116 +7,89 @@ local xp5 = 825 -- 50-59
 local xp6 = 1500 -- 60-69
 local xp7 = 2000 -- 70-79
 
-function OnCreatureKill(event, killer, killed)
-local plevel = killer:GetLevel()
-local creatureentry = killed:GetEntry()
-local creaturename = killed:GetName()
+
+--function OnCreatureKill(event, player, victim)
+function OnGiveXP(event, player, amount, victim)
+local plevel = player:GetLevel()
+if victim ~= nil then
+local creatureentry = victim:GetEntry()
+local creaturename = victim:GetName()
 local gold = 10000
-local Map = killer:GetMap()
+local Map = player:GetMap()
 local inDungeon = Map:IsDungeon()
-local name = killer:GetName()
+local name = player:GetName()
 
 
 
 local number = math.random(1, 20)
 if not inDungeon then
-	local x = killed:GetX()
-	local y = killed:GetY()
-	local z = killed:GetZ()
-	local o = killed:GetO()
+	local x = victim:GetX()
+	local y = victim:GetY()
+	local z = victim:GetZ()
+	local o = victim:GetO()
 	local spawnedCreature
 	
 	local spawnlevel = plevel+1
 	
 	if number == 4 then
-	spawnedCreature = killed:SpawnCreature( creatureentry, x+1, y+1, z+0.5, o-3.5, 1, 60  )
+	spawnedCreature = victim:SpawnCreature( creatureentry, x+1, y+1, z+0.5, o-3.5, 1, 60  )
 	spawnedCreature:SetLevel( spawnlevel )
 	end
 	if number == 7 then
-	spawnedCreature = killed:SpawnCreature( creatureentry, x+1, y+1, z+0.5, o-3.5, 1, 60  )
+	spawnedCreature = victim:SpawnCreature( creatureentry, x+1, y+1, z+0.5, o-3.5, 1, 60  )
 	spawnedCreature:SetLevel( spawnlevel )
-	spawnedCreature = killed:SpawnCreature( creatureentry, x+2, y+2, z+0.5, o-3.5, 1, 60  )
+	spawnedCreature = victim:SpawnCreature( creatureentry, x+2, y+2, z+0.5, o-3.5, 1, 60  )
 	spawnedCreature:SetLevel( spawnlevel )
 
 	end
 	if number == 9 then
-	spawnedCreature = killed:SpawnCreature( creatureentry, x+1, y+1, z+0.5, o-3.5, 1, 60  )
+	spawnedCreature = victim:SpawnCreature( creatureentry, x+1, y+1, z+0.5, o-3.5, 1, 60  )
 	spawnedCreature:SetLevel( spawnlevel )
-	spawnedCreature = killed:SpawnCreature( creatureentry, x+2, y+2, z+0.5, o-3.5, 1, 60  )
+	spawnedCreature = victim:SpawnCreature( creatureentry, x+2, y+2, z+0.5, o-3.5, 1, 60  )
 	spawnedCreature:SetLevel( spawnlevel )
-	spawnedCreature = killed:SpawnCreature( creatureentry, x+3, y+3, z+0.5, o-3.5, 1, 60  )
+	spawnedCreature = victim:SpawnCreature( creatureentry, x+3, y+3, z+0.5, o-3.5, 1, 60  )
 	spawnedCreature:SetLevel( spawnlevel )
 	end
 	
 
 if number == 2 or number == 15 or number == 14 or number == 1 then
-killer:ModifyMoney( gold*1 )
-killer:SendBroadcastMessage("|cff5af304You recieved an extra 1 gold from killing " ..creaturename.."|r")
+player:ModifyMoney( gold*1 )
+player:SendBroadcastMessage("|cff5af304You recieved an extra 1 gold from killing " ..creaturename.."|r")
 end
 
 if number == 6 then
-killer:ModifyMoney( gold*2 )
-killer:SendBroadcastMessage("|cff5af304You recieved an extra 2 gold from killing " ..creaturename.."|r")
+player:ModifyMoney( gold*2 )
+player:SendBroadcastMessage("|cff5af304You recieved an extra 2 gold from killing " ..creaturename.."|r")
 end
 
 
 if number == 8 then
-killer:ModifyMoney( gold*3 )
-killer:SendBroadcastMessage("|cff5af304You recieved an extra 3 gold from killing " ..creaturename.."|r")
+player:ModifyMoney( gold*3 )
+player:SendBroadcastMessage("|cff5af304You recieved an extra 3 gold from killing " ..creaturename.."|r")
 end
 
 if number == 16 then
-killer:ModifyMoney( gold*10 )
-killer:SendBroadcastMessage("|cff5af304You recieved an extra 3 gold from killing " ..creaturename.."|r")
+player:ModifyMoney( gold*10 )
+player:SendBroadcastMessage("|cff5af304You recieved an extra 3 gold from killing " ..creaturename.."|r")
 end
 
 if number == 3 then
-	if plevel <= 19 then
-	bonus1 = xp1*plevel
-	killer:GiveXP( bonus1 )
-	killer:SendBroadcastMessage("|cff5af304You recieved an extra " ..bonus1..  "xp from killing " ..creaturename.."|r")
-	end
-	if plevel >= 20 and plevel <= 29 then
-	bonus2 = xp2*plevel
-	killer:GiveXP( bonus2 )
-	killer:SendBroadcastMessage("|cff5af304You recieved an extra " ..bonus2..  "xp from killing " ..creaturename.."|r")
-	end
-	if plevel >= 30 and plevel <= 39 then
-	bonus3 = xp3*plevel
-	killer:GiveXP( bonus3 )
-	killer:SendBroadcastMessage("|cff5af304You recieved an extra " ..bonus3..  "xp from killing " ..creaturename.."|r")
-	end
-	if plevel >= 40 and plevel <= 49 then
-	bonus4 = xp4*plevel
-	killer:GiveXP( bonus4 )
-	killer:SendBroadcastMessage("|cff5af304You recieved an extra " ..bonus4..  "xp from killing " ..creaturename.."|r")
-	end
-	if plevel >= 50 and plevel <= 59 then
-	bonus5 = xp15*plevel
-	killer:GiveXP( bonus5 )
-	killer:SendBroadcastMessage("|cff5af304You recieved an extra " ..bonus5..  "xp from killing " ..creaturename.."|r")
-	end
-	if plevel >= 60 and plevel <= 69 then
-	bonus16 = xp6*plevel
-	killer:GiveXP( bonus6 )
-	killer:SendBroadcastMessage("|cff5af304You recieved an extra " ..bonus6..  "xp from killing " ..creaturename.."|r")
-	end
-	if plevel >= 70 and plevel <= 79 then
-	bonus7 = xp7*plevel
-	killer:GiveXP( bonus1, 60  )
-	killer:SendBroadcastMessage("|cff5af304You recieved an extra " ..bonus7..  "xp from killing " ..creaturename.."|r")
-	end
+	 --if victim ~= nil then
+	bonus = ammount*10
+	player:GiveXP( bonus1 )
+	player:SendBroadcastMessage("|cff5af304You recieved an extra " ..bonus..  "xp from killing " ..creaturename.."|r")
+	--end
 	
 end
 
 if number == 12 then
-killer:ModifyMoney( -gold*5 )
-killer:SendBroadcastMessage("|cff5af304You lost 5 gold.|r")
+player:ModifyMoney( -gold*5 )
+player:SendBroadcastMessage("|cff5af304You lost 5 gold.|r")
 end
 
 
 if number == 17 then
-spawnedCreature = killed:SpawnCreature( 14273, x+3, y+3, z+0.5, o-3.5, 1, 60  )
+spawnedCreature = victim:SpawnCreature( 14273, x+3, y+3, z+0.5, o-3.5, 1, 60  )
 spawnedCreature:SetLevel( plevel )
 end
 
@@ -126,24 +99,13 @@ end
    
 	
 
-
+end
 end
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 if enabled then
-RegisterPlayerEvent(7, OnCreatureKill)
+--RegisterPlayerEvent(7, OnCreatureKill)
+RegisterPlayerEvent(12, OnGiveXP)
 end
